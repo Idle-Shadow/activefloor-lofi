ce6077d2728be47ec0d4562106ea271705af1007
Jeffrey Derksen <74195106+jeffrey-fontys@users.noreply.github.com>
2022-12-21 20:24:45 +0100

    Update LavaMaterialSwitcher.cs
    Now works with any number of materials and works in sync with game timer.

--- Assets/Scripts/LavaMaterialSwitcher.cs
+++ Assets/Scripts/LavaMaterialSwitcher.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +7,35 @@ public class LavaMaterialSwitcher : MonoBehaviour
     [SerializeField] Timer _timer;
     [SerializeField] MeshRenderer _meshRenderer;
 
-    int index = 0;
+    float[] _timeStamps;
+    int _index = 0;
 
     void Start()
     {
+        _timeStamps = new float[_materials.Count - 1];
+        _timeStamps[0] = 0;
+        for (int i = 1; i < _timeStamps.Length; i++)
+        {
+            float timeStamp = _timer.MaxTime / (_materials.Count - 1) * i;
+            _timeStamps[i] = timeStamp;
+        }
+
         _meshRenderer.material = _materials[0];
-        StartCoroutine(Next());
     }
 
     void Update()
     {
-
-    }
-
-    IEnumerator Next()
-    {
-        while (index < 5)
+        if (_timer.TimeRemaining == 0)
         {
-            yield return new WaitForSeconds(5);
-            index++;
-            _meshRenderer.material = _materials[index];
+            _meshRenderer.material = _materials[_materials.Count - 1];
+        }
+        else
+        {
+            for (int i = 0; i < _timeStamps.Length; i++)
+            {
+                if (_timer.TimeRemaining > _timeStamps[i]) _index = i + 2;
+            }
+            _meshRenderer.material = _materials[_materials.Count - _index];
         }
     }
 }
